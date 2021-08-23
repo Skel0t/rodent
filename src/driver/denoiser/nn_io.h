64eb66c580a957d2492c29255dd799038567e52d
s8jjmeye <s8jjmeye@stud.uni-saarland.de>
2021-08-23 20:31:42 +0200

    include simple sres convolution benchmark

--- src/driver/denoiser/nn_io.h
+++ src/driver/denoiser/nn_io.h
@@ -1,8 +1,9 @@
-#ifndef NN_H
-#define NN_H
+#ifndef NN_IO_H
+#define NN_IO_H
 
 void read_in_weigths_chw(float* buffer, int offset, std::string path, int in_channels, int out_channels, int ksize);
 void read_in_weigths_hwc(float* buffer, int offset, std::string path, int in_channels, int out_channels, int ksize);
+void read_in_matrix_chw(float* buffer, std::string path, int channels, int rows, int cols);
 void read_in_biases(float* buffer, int offset, std::string path, int out_channels);
 float* read_in_biases2(std::string path, int out_channels);
 
